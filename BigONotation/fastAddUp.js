febbd0353e842d82ec8c6b9193f784212fd02175
unknown <bano.nagma.1@gmail.com>
2019-10-12 14:59:21 +0530

    fast addition

---
+++ BigONotation/fastAddUp.js
@@ -0,0 +1,11 @@
+var sumFast = '';
+var timeSlow;
+
+function addUpTo(n){
+    return n*(n+1)/2;
+}
+
+let t1 = performance.now();
+sumFast = addUpTo(1000000000);
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1)/1000} seconds.`);
--- BigONotation/slowAddUp.js
+++ BigONotation/slowAddUp.js
@@ -1,16 +1,17 @@
 // slow add up to
+var sumSlow = '';
+var timeFast;
 
 function addUpTo(n) {
     let total = 0;
     for (let i = 1 ; i <= n ; i++) {
-        total += i
+        total += i;
     }
 
     return total
 }
 
 let time1 = performance.now();
-addUpTo(1000000000);
+sumSlow = addUpTo(1000000000);
 let time2 = performance.now();
 console.log(`Time Elapsed: ${(time2 - time1)/1000} seconds.`);
-
