729ba3f506a3f3711dc32643258b510c60571a1a
unknown <bano.nagma.1@gmail.com>
2019-10-27 00:17:16 +0530

    better approach

--- ProblemSolving/charCount.js
+++ ProblemSolving/charCount.js
@@ -65,3 +65,42 @@ arr1 = [1,2,1];
 arr2 = [4,4,1];
 
 console.log(isSame(arr1, arr2));
+
+// a better approach for isSame() by reducing the time complexity
+
+function isSameBetter(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+
+    let frequencyCounter1 = {};
+    let frequencyCounter2 = {};
+
+    for (let val of arr1) {
+        frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+    }
+    console.log(frequencyCounter1);
+
+    for(let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    console.log(frequencyCounter2);
+
+    for (let key in frequencyCounter1) {
+        if (!((key * key) in frequencyCounter2)) {
+            return false;
+        }
+        if (frequencyCounter1[key] !== frequencyCounter2[key * key]) {
+            return false;
+        }
+    }
+    
+    return true;
+    
+}
+
+a = [1,1,2,5];
+b = [4,1,25,1];
+
+console.log(isSameBetter(a, b));
