05428dbd77efa303e1ca9e019294247e78431640
unknown <bano.nagma.1@gmail.com>
2019-10-27 15:15:23 +0530

    sliding window

--- ProblemSolving/maxSubarraySum.js
+++ ProblemSolving/maxSubarraySum.js
@@ -1,4 +1,5 @@
 // max sum of subarray
+// naive solution
 
 function maxSubarraySum(arr, n) {
     if (n > arr.length) {
@@ -19,6 +20,31 @@ function maxSubarraySum(arr, n) {
 }
 
 arr1 = [1,2,3,3,4,7,2,1,0];
-
 console.log('subarray sum max', maxSubarraySum(arr1,3));
+
+
+// sliding window - better solution
+
+function maxSubarraySumBetter(arr, n) {
+    if (arr.length < n) {
+        return null;
+    }
+    let tempSum = 0;
+    let maxSum = 0;
+    for (let i = 0 ; i < n ; i++) {
+        tempSum += arr[i];
+    }
+    maxSum = tempSum;
+    for (let i = n ; i < arr.length ; i++) {
+        tempSum = tempSum - arr[i - n] + arr[n];
+        maxSum = Math.max(tempSum, maxSum);
+    }
+
+    return maxSum;
+
+}
+
+arr2 = [1,1,2,3,4,2,1,3];
+
+console.log('subarray sum max better', maxSubarraySum(arr2,3));
 
